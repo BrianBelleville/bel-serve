acc6731477628cb095b32dd928d9bdf4a9a32a6f
BrianBelleville <b.belleville@gmail.com>
2012-12-08 18:43:17 -0800

    closing the tags on the static responses

--- static-responses.lisp
+++ static-responses.lisp
@@ -1,42 +1,57 @@
 (in-package :bel-serve)
 
-
 (defvar *error-response*
   (create-response *error*
 		   "<!DOCTYPE HTML PUBLIC \"-//IETF//DTD HTML 2.0//EN\">
-<html><head>
+<html>
+<head>
 <title>500 Internal Server Error</title>
-</head><body>
+</head>
+<body>
 <h1>Not Found</h1>
 <p>The server has encountered an internal error</p>
-<hr>"))
+<hr>
+</body>
+</html>"))
 
 (defvar *not-found-response*
   (create-response *not-found*
 		   "<!DOCTYPE HTML PUBLIC \"-//IETF//DTD HTML 2.0//EN\">
-<html><head>
+<html>
+<head>
 <title>404 Not Found</title>
-</head><body>
+</head>
+<body>
 <h1>Not Found</h1>
 <p>The requested URL was not found on this server.</p>
-<hr>"))
+<hr>
+</body>
+</html>"))
 
 (defvar *bad-request-response*
   (create-response *bad-request*
 		   "<!DOCTYPE HTML PUBLIC \"-//IETF//DTD HTML 2.0//EN\">
-<html><head>
+<html>
+<head>
 <title>400 Bad Request</title>
-</head><body>
+</head>
+<body>
 <h1>Bad Request</h1>
 <p>The request was not a properly formed HTTP request</p>
-<hr>"))
+<hr>
+</body>
+</html>"))
 
 (defvar *forbidden-response*
   (create-response *forbidden*
 		   "<!DOCTYPE HTML PUBLIC \"-//IETF//DTD HTML 2.0//EN\">
-<html><head>
+<html>
+<head>
 <title>403 Forbidden</title>
-</head><body>
+</head>
+<body>
 <h1>Forbidden</h1>
 <p>You are forbidden from accessing the requested URL on this server.</p>
-<hr>"))
+<hr>
+</body>
+</html>"))
