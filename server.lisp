e2eb4ed2ab8c65445c46254d31ddd5ddd1100e3c
BrianBelleville <b.belleville@gmail.com>
2013-02-01 19:07:06 -0800

    fixed formatting in the file server.lisp

--- server.lisp
+++ server.lisp
@@ -8,7 +8,10 @@
     (if (equal uri "/")
 	(probe-file *root-file-path*)
 	(let ((file (probe-file (append-path (probe-file *root-dir*) uri))))
-	  (if (and file (pathname-name file)) ; if the file is found, and the result of pathname-name is not nil, it is assumed to be a regular file and not a directory
+	  (if (and file (pathname-name file))
+	      ;; if the file is found, and the result of pathname-name
+	      ;; is not nil, it is assumed to be a regular file and
+	      ;; not a directory
 	      file)))))
 
 (defun set-content-type (response resource-pathname)
@@ -19,7 +22,11 @@
 	  (setf (gethash :content-type headers) val)
 	  (error "unsupported content type")))))
 
-;; todo : send-resource and send-string should probably be the same generic function with dispatch on type, so that the sending of dynamically generated content and static content uses the same function. This could also allow for handlers to return different types of data than a string
+;; todo : send-resource and send-string should probably be the same
+;; generic function with dispatch on type, so that the sending of
+;; dynamically generated content and static content uses the same
+;; function. This could also allow for handlers to return different
+;; types of data than a string
 (defun send-resource (stream  pathname)
   (declare (type stream stream))
   (let ((response (make-ok-response)))
@@ -54,15 +61,19 @@
 
 (defun service-request (remote-socket)
   (multiple-value-bind (request stream) (receive-request remote-socket)
-    (declare (type stream stream)) ; request may be null if there is an error with what was read from the stream
+    (declare (type stream stream)) ;; request may be null if there is
+				   ;; an error with what was read from
+				   ;; the stream
     (handler-case
-	(if request
-	    ;right now only suport GET requests, in the future maybe split out this cond clause to handle the variety of request types.
+	(if request ;; right now only suport GET requests, in the
+		    ;; future maybe split out this cond clause to
+		    ;; handle the variety of request types.
 	    (cond ((equal (http-request-method request) "GET")
 		   (service-get request stream))
 		  (t (send-response stream *forbidden-response*)))
 	    (send-response stream *bad-request-response*))
-      (socket-error () ) ; socket error, do nothing, socket will be closed after the form is exited
+      (socket-error () ) ;; socket error, do nothing, socket will be
+			 ;; closed after the form is exited
       (error () (send-response stream *error-response*)))
     (close stream)))
 
@@ -84,15 +95,20 @@
 	   (let ((remote-socket (with-mutex (*con-queue-mutex*)
 				  (dequeue *connection-queue*))))
 	     (service-request remote-socket)))
-       (error () )))) ; do nothing for now, prevents an unhandled error from stopping the loop by entering the debugger. todo: put in some sort of logging
+       (error () )))) ;; do nothing for now, prevents an unhandled
+		      ;; error from stopping the loop by entering the
+		      ;; debugger. todo: put in some sort of logging
 
 (defun spawn-service-thread (name)
   (declare (type string name))
   (make-thread #'service-connections-loop
 			:name name))
        
-(defun start-server (&optional (port 8080) (root-dir *root-dir*) (root-file-path *root-file-path*))
-  "starts an http server listening at port. It will serve files from root-dir, and requests for root will be answered with the file at root-file-path"
+(defun start-server
+    (&optional (port 8080) (root-dir *root-dir*) (root-file-path *root-file-path*))
+  "starts an http server listening at port. It will serve files from
+root-dir, and requests for root will be answered with the file at
+root-file-path"
   (setf *root-dir* root-dir
 	*root-file-path* root-file-path)
   (let ((listen-socket (make-instance 'inet-socket :type :stream :protocol :tcp)))
@@ -104,4 +120,4 @@
       (loop
 	 for i from 1 to *number-of-threads* do
 	  (spawn-service-thread (format nil "Service Connections Thread ~D" i)))))
-  t) ;return t
+  t) ;;return t
